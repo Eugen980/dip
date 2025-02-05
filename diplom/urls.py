ffa351625a9f7e221146cd23a8c997c0bbfcc5aa
Eugen980 <zaza980q@mail.ru>
2025-02-05 16:53:08 +0300

    3

--- diplom/urls.py
+++ diplom/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for diplom project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
@@ -25,4 +9,10 @@ urlpatterns = [
     path('', v.index, name='main'),
     path('services', views.ServiceListView.as_view(), name='services_list'),
     path('create_service', views.ServiceCreateView.as_view(), name='service_create'),
+    path('create_client', views.ClientCreateView.as_view(), name='client_create'),
+    path('clients', views.ClientListView.as_view(), name='client_list'),
+    path('clients/<int:pk>', views.ClientShowView.as_view(), name='client_show'),
+    path('clients/<int:pk>/delete', views.ClientDeleteView.as_view(), name='client_delete'),
+    path('clients/<int:pk>/update', views.ClientUpdateView.as_view(), name='client_update'),
 ]
+
